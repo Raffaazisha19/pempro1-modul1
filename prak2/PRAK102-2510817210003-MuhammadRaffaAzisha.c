1bd59a4ca2ab626ed268e1c3bfc521d60803ee40
Raffaazisha19 <rapaajis@gmail.com>
2025-10-02 07:51:43 +0800

    Update PRAK102-2510817210003-MuhammadRaffaAzisha.c

--- prak2/PRAK102-2510817210003-MuhammadRaffaAzisha.c
+++ prak2/PRAK102-2510817210003-MuhammadRaffaAzisha.c
@@ -4,9 +4,9 @@ int main()
     double a = 4;
     double b = 8;
     double c = 3;
-    printf("Variabel X bernilai %.0f\n", a);
-    printf("Variabel Y bernilai %.0f\n", b);
-    printf("Variabel Z bernilai %.0f\n", c);
+    printf("Variabel A bernilai %.0f\n", a);
+    printf("Variabel B bernilai %.0f\n", b);
+    printf("Variabel C bernilai %.0f\n", c);
     printf("Jumlah variabel tersebut adalah %f", a*b/c );
     return 0;
 }
